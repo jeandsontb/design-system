6477aa1b4f7acbbdbb46cfccd933aa7038681fa6
jeandson <jeandsontb@gmail.com>
2023-02-09 21:44:31 -0300

    initial commit

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import type { StoryObj, Meta } from "@storybook/react";
+import { Button, ButtonProps } from "@design-ui/react";
+
+export default {
+  title: "Form/Button",
+  component: Button,
+
+  args: {
+    children: "Enviar",
+  },
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: "big",
+  },
+};
