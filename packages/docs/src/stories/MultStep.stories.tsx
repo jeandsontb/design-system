7d9d8f79326b2631636fb778d892001e7a2cd0d3
jeandson <jeandsontb@gmail.com>
2023-02-10 21:15:59 -0300

    finish design system

---
+++ packages/docs/src/stories/MultStep.stories.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import type { StoryObj, Meta } from "@storybook/react";
+import { Box, MultStep, MultStepProps } from "@design-ui/react";
+
+export default {
+  title: "Form/Mult Step",
+  component: MultStep,
+  args: {
+    size: 4,
+    currentStep: 1,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: "flex", flexDirection: "column", gap: "$2" }}
+        >
+          {Story()}
+        </Box>
+      );
+    },
+  ],
+} as Meta<MultStepProps>;
+
+export const Primary: StoryObj<MultStepProps> = {
+  args: {},
+};
+
+export const Full: StoryObj<MultStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+};
---
+++ packages/react/src/components/MultStep/styles.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { styled } from "../../styles";
+import { Text } from "../Text";
+
+export const MultStepContainer = styled("div", {});
+
+export const Label = styled(Text, {
+  color: "$gray200",
+
+  defaultVariants: {
+    fontSize: "$xs",
+  },
+});
+
+export const Steps = styled("div", {
+  display: "grid",
+  gridTemplateColumns: "repeat(var(--steps-size), 1fr)",
+  gap: "$2",
+  marginTop: "$1",
+});
+
+export const Step = styled("div", {
+  height: "$1",
+  borderRadius: "$px",
+  backgroundColor: "$gray600",
+
+  variants: {
+    active: {
+      true: {
+        backgroundColor: "$gray100",
+      },
+    },
+  },
+});
